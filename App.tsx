8af55eb653e66991f8f609cf3ac1d597f7a61847
Vadim Snitkovsky <vadim.snitkovsky@gmail.com>
2023-10-27 10:48:57 -0400

    review changes

--- App.tsx
+++ App.tsx
@@ -1,22 +1,18 @@
 
-import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
 import Slider from './src/components/Slider';
 import AnimatedCarousel from './src/components/AnimatedCarousel';
 import data from './src/data';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const App = ({ carouselRef }) => {
+const App = () => {
   const useOldCode = false;
   const CarouselType = useOldCode ? Slider : AnimatedCarousel;
 
   return (
-
-<View >
+    <View >
       <CarouselType slides={data} />
-</View>
-
-
+    </View>
   );
 };
 
