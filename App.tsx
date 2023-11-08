fd421d809d174d4aa1183cd6abe855f877eba504
lawankar.anuja08@gmail.com <lawankar.anuja08@gmail.com>
2023-11-08 09:08:39 -0500

    Cleanup unwanted code

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import {View} from 'react-native';
 import React from 'react';
-import Slider from './src/components/Slider';
+
 import AnimatedCarousel from './src/components/AnimatedCarousel';
 import data from './src/data';
 
 const App = () => {
-  const useOldCode = false;
-  const CarouselType = useOldCode ? Slider : AnimatedCarousel;
 
+  const CarouselType = AnimatedCarousel;
   //-- VS: if you want to use View here, which you might, you need to have flex:1 here
   return (
     <>
